a9f4cc7a4aaa5783153c8b7c2fb7caa576955b45
JakkuSakura <qjk2001@gmail.com>
2026-01-06 11:52:47 +0800

    refactor: unify quote types under TypeQuote

--- crates/fp-wit/src/serializer.rs
+++ crates/fp-wit/src/serializer.rs
@@ -678,21 +678,7 @@ impl InterfaceBuilder {
             Ty::Expr(expr) => {
                 expr_to_wit_type(expr, receiver_scope).unwrap_or_else(|| "json".to_string())
             }
-            Ty::QuoteExpr(_)
-            | Ty::QuoteStmt(_)
-            | Ty::QuoteItem(_)
-            | Ty::QuoteFn(_)
-            | Ty::QuoteStruct(_)
-            | Ty::QuoteEnum(_)
-            | Ty::QuoteTrait(_)
-            | Ty::QuoteImpl(_)
-            | Ty::QuoteConst(_)
-            | Ty::QuoteStatic(_)
-            | Ty::QuoteMod(_)
-            | Ty::QuoteUse(_)
-            | Ty::QuoteMacro(_)
-            | Ty::QuoteType(_)
-            | Ty::QuoteToken(_) => "json".to_string(),
+            Ty::Quote(_) => "json".to_string(),
         }
     }
 
@@ -1475,21 +1461,7 @@ fn ty_to_wit_with_self(ty: &Ty, self_name: Option<&str>) -> String {
             let inner = ty_to_wit_with_self(vec_ty.ty.as_ref(), self_name);
             format!("list<{inner}>")
         }
-        Ty::QuoteExpr(_)
-        | Ty::QuoteStmt(_)
-        | Ty::QuoteItem(_)
-        | Ty::QuoteFn(_)
-        | Ty::QuoteStruct(_)
-        | Ty::QuoteEnum(_)
-        | Ty::QuoteTrait(_)
-        | Ty::QuoteImpl(_)
-        | Ty::QuoteConst(_)
-        | Ty::QuoteStatic(_)
-        | Ty::QuoteMod(_)
-        | Ty::QuoteUse(_)
-        | Ty::QuoteMacro(_)
-        | Ty::QuoteType(_)
-        | Ty::QuoteToken(_) => "json".to_string(),
+        Ty::Quote(_) => "json".to_string(),
         Ty::Array(array) => {
             let inner = ty_to_wit_with_self(array.elem.as_ref(), self_name);
             format!("list<{inner}>")
